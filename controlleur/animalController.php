e3231ad34775323db28524e9caaafa2153945709
manue0203 <manueng2006@gmail.com>
2025-10-06 16:04:56 +0200

    Update animalController.php

--- controlleur/animalController.php
+++ controlleur/animalController.php
@@ -2,6 +2,7 @@
 declare(strict_types=1);
 require_once 'model/animaux.php';
 session_start();
+
 class animalController{
     private animaux $animal;
 
@@ -9,10 +10,12 @@ class animalController{
         $this->animal = new Animaux(); 
     }
     
-    
     function addAnimal($type, $nom, $age, $description, $statut){
         if (empty($type)||empty($nom)||empty($age)||empty($description)||empty($statut)){
-        
+            return false;
         }
+        return $this->animal->addAnimal($type, $nom, $age, $description, $statut);
+
     }
+
 }
