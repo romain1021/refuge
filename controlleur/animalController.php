0c178d9c36ed2560661816c6fd9f515c0d432953
manue0203 <manueng2006@gmail.com>
2025-10-06 15:33:53 +0200

    UserController.php

---
+++ controlleur/animalController.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+require_once 'model/animaux.php';
+session_start();
+class animalController{
+    private animaux $animal;
+
+    function __construct(){
+        $this->animal = new Animaux(); 
+    }
+    
+    
+    function addAnimal($type, $nom, $age, $description, $statut){
+        if (empty($type)||empty($nom)||empty($age)||empty($description)||empty($statut)){
+        
+        }
+    }
+}
