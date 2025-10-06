8ff90597f3ea19625ee96cf4f58d66f65365e93c
manue0203 <manueng2006@gmail.com>
2025-10-06 14:58:44 +0200

    Update UserController.php

--- controlleur/UserController.php
+++ controlleur/UserController.php
@@ -1,21 +1,54 @@
-<?php 
-require_once("../Models/user.php");
+<?php
+declare(strict_types=1);
+require_once 'model/User.php';
 session_start();
 
 class UserController{
-    function login($identifiants, $mdp){
-        $user= getPasswordfromUser($identifiants);
+    private User $user;
 
-        if(!$user){
-            return false;
+    public function __construct(){
+        $this->user = new User(); 
+    }
+    
+    public function login(string $username, string $password){
+        
+        $isValid = $this->user->testUser($username, $password);
+
+        if ($isValid) {
+            $userId = $_COOKIE['id_user'];
+
+            if ($userId) {
+                $data = $this->user->fetchUserData($userId);
+                $this->user = new User($data); 
+
+                $_SESSION['id_user'] = $this->user->getId();
+                $_SESSION['statut'] = $this->user->getStatut(); 
+
+                return true;
+            } 
         }
 
-        if(password_verify($mdp, $user['password'])){
-            $_SESSION['id_user']=$user['id'];
-            $_SESSION['statut_user']=$user['statut'];
-            return true;
-        }
         return false;
     }
 
+
+    function register($nom,$prenom,$email,$password,$adresse){
+        if (empty($nom) || empty($prenom) || empty($email) || empty($password) || empty($adresse)) {
+            return false;
+        }
+
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            return false;
+        }
+
+    }
+    
+    function getUserById(){
+        if (isset($_SESSION['id_user'])) {
+            $data = $this->user->fetchUserData($_SESSION['id_user']);
+            return new User($data);
+        }
+    }
 }
+
+    
