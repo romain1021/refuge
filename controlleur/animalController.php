738cea251ef2e5d84564b7354805dc43691dcddc
manue0203 <manueng2006@gmail.com>
2025-10-13 15:15:35 +0200

    Update animalController.php

--- controlleur/animalController.php
+++ controlleur/animalController.php
@@ -13,34 +13,36 @@ class AnimalController {
         $this->animal = new Animaux();
     }
 
-    function addAnimal($type, $nom, $age, $description, $statut) {
-        if (empty($type) || empty($nom) || empty($age) || empty($description) || empty($statut)) {
+    function addAnimal(Animaux $animal) {
+        if (empty($animal->getType()) || empty($animal->getNom()) || empty($animal->getAge()) || empty($animal->getDescription()) || empty($animal->getStatut())) {
             return false; 
         }
         else{
             $result = $this->conn->prepare("INSERT INTO animaux (type, nom, age, description, statut)VALUES (:type, :nom, :age, :description, :statut)");
-            $result->bindParam(':type', $type);
-            $result->bindParam(':nom', $nom);
-            $result->bindParam(':age', $age);
-            $result->bindParam(':description', $description);
-            $result->bindParam(':statut', $statut);
-            return $result->execute();
+            $result->bindParam(':type', $animal->getType());
+            $result->bindParam(':nom', $animal->getNom());
+            $result->bindParam(':age', $animal->getAge());
+            $result->bindParam(':description', $animal->getDescription());
+            $result->bindParam(':statut', $animal->getStatut());
+            $result->execute();
+            return $animal;
         }
         
     }
 
-    function editAnimal($id,$type, $nom, $age, $description, $statut) {
+    function editAnimal(Animaux $animal) {
         $result = $this->conn->prepare("UPDATE animaux SET type = :type, nom = :nom, age = :age, description = :description, statut = :statut WHERE id = :id");
-        $result->bindParam(':type',$type);
-        $result->bindParam(':nom',$nom);
-        $result->bindParam(':age',$age);
-        $result->bindParam(':description',$description);
-        $result->bindParam(':statut',$statut);        
-        return $result->execute();
+        $result->bindParam(':type',$animal->getType());
+        $result->bindParam(':nom',$animal->getNom());
+        $result->bindParam(':age',$animal->getAge());
+        $result->bindParam(':description',$animal->getDescription());
+        $result->bindParam(':statut',$animal->getStatut());        
+        $result->execute();
+        return $animal;
     }
 
     function changerStatut($id) {
-        $result = $this->conn->prepare("SELECT statut FROM animaux WHERE id = $id");
+        $result = $this->conn->prepare("SELECT statut FROM animaux WHERE id = :id");
         $result->bindParam(':id',$id);
         $result->execute();
         $statutActuel = $result->fetchColumn();
@@ -56,7 +58,9 @@ class AnimalController {
         $modif->bindParam(':statut', $nouveauStatut);
         $modif->bindParam(':id', $id);
         $modif->execute();
+        return $nouveauStatut;
     }
+
 
 }
 
