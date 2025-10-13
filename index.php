fc4655b3d3de2bd7b1368377ac4495f774b09dd9
romain1021 <150360051+romain1021@users.noreply.github.com>
2025-10-13 16:10:33 +0200

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,8 @@ require_once("controlleur/UserController.php");
 require_once("controlleur/animalController.php");
 require_once("model/animaux.php");
 require_once("model/user.php");
-
+session_abort();
+session_start();
 
 if(isset($_GET['page'])){
     if ($_GET['page']=="home"){
@@ -18,7 +19,23 @@ if(isset($_GET['page'])){
         echo'test';
     }
 
+    if ($_GET['page']=='connexion'){
+        require_once('vue/login.php');
+    }
+
     if ($_GET['page']=='profil'){
+        require_once('vue/vueProfilAdoptant.php');
+    }
+
+    if ($_GET['page']=='registerAnimal'){
+        require_once('vue/registerAnimal.php');
+    }
+
+    if ($_GET['page']=='viewAnimal'){
+        require_once('vue/viewAnimal.php');
+    }
+
+    if ($_GET['page']=='vueProfilAdoptant'){
         require_once('vue/vueProfilAdoptant.php');
     }
 
@@ -26,5 +43,5 @@ if(isset($_GET['page'])){
 }
 
 else{
-   // require("vue/login.php");
+   $_get=='connexion';
 }
