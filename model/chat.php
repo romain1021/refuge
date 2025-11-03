00aa3d7da4686567c162167c4fdbe127e0bb4d87
romain1021 <150360051+romain1021@users.noreply.github.com>
2025-11-03 14:27:23 +0100

    fix

--- model/chat.php
+++ model/chat.php
@@ -3,10 +3,6 @@ require_once __DIR__ . '/animaux.php';
 
 class Chat extends Animaux
 {
-    // Race choisie pour l'instance (optionnelle)
-    protected ?string $race = null;
-
-    // Liste statique des races de chats disponibles
     public static array $Listeraces = ['Siamois','Persan','Maine Coon','British Shorthair','Bengal','Sphynx','Ragdoll','Norvégien','Européen','Scottish Fold'
     ];
 
--- model/chien.php
+++ model/chien.php
@@ -3,10 +3,6 @@ require_once __DIR__ . '/animaux.php';
 
 class Chien extends Animaux
 {
-    // Race choisie pour l'instance (optionnelle)
-    protected ?string $race = null;
-
-    // Liste statique des races de chiens disponibles
     public static array $races = ['Labrador','Golden Retriever','Berger Allemand','Bouledogue Français','Beagle','Teckel','Yorkshire','Border Collie','Bichon Frisé','Rottweiler'];
 
     public static function getRaces(): array{
